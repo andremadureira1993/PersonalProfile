b9f2259f5244dafb58da810e46ca57cdea75db11
André Moraes <andre_moraes@hst.com.br>
2021-01-17 10:55:40 -0300

    Scretch front

---
+++ Front/personalprofile/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Home />
+      <Submenu />
+    </>
+  );
+}
+
+export default App;
